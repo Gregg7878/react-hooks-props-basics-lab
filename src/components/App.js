ada168bd47d320ebaf656018715eec9b441a2c9f
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-03-01 11:42:29 +0300

    modified App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,17 @@ import About from "./About";
 import user from "../data/user";
 
 const App = () => {
-  return (
-    <div>
-      <NavBar />
-      <div id="home">
-        <Home username={user.name} city={user.city} color={user.color} />
-      </div>
-      <div id="about">
-        <About bio={user.bio} links={user.links} />
-      </div>
-    </div>
-  );
+return (
+<div>
+<NavBar />
+<div id="home">
+<Home username={user.name} city={user.city} color={user.color} />
+</div>
+<div id="about">
+<About bio={user.bio} links={user.links} github={user.links.github} linkedin={user.links.linkedin} />
+</div>
+</div>
+);
 };
 
 export default App;
